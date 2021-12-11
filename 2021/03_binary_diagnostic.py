e5480e3aa66ef88c7508472ce52dc863812bc176
Ian Dimayuga <git@iandimayuga.com>
2021-12-11 00:43:50 -0800

    [Day 3 Part 2] Perform the O2/CO2 diagnostic binary search.

--- 2021/03_binary_diagnostic.py
+++ 2021/03_binary_diagnostic.py
@@ -1,32 +1,61 @@
 _DATA_FILE = "2021/data/03_binary_diagnostic.txt"
-_BIT_LENGTH = 12
+_BIT_LENGTH = 12 
 
 numlist = []
 with open(_DATA_FILE, "r") as input:
   numlist = list(map(lambda s : int(s, base=2), input.readlines()))
 
-# Maintain a count for each bit.
-bit_counts = [0] * _BIT_LENGTH
+# Treat it like a binary search.
+# Keep a running number starting with the largest power of 2.
+o2_pivot = 0
+co2_pivot = 0
 
-# +1 for 1-bits, and -1 for 0-bits.
-for num in numlist:
-  for bit in range(0, _BIT_LENGTH):
-    if (num & 2**bit):
-      bit_counts[bit] += 1
+o2_list = numlist.copy()
+co2_list = numlist.copy()
+for bit in reversed(range(0, _BIT_LENGTH)):
+  if (len(o2_list) > 1):
+    # Set the bit to 1 in the value for comparison.
+    o2_pivot += 2**bit
+
+    o2_1s = []
+    o2_0s = []
+    for num in o2_list:
+      if (num >= o2_pivot):
+        o2_1s.append(num)
+      else:
+        o2_0s.append(num)
+    
+    if (len(o2_1s) >= len(o2_0s)):
+      # 1s are more common, leave the 1 in the pivot.
+      o2_list = o2_1s
     else:
-      bit_counts[bit] -= 1
+      # 0s are more common, set the bit to 0 in the pivot.
+      o2_pivot -= 2**bit
+      o2_list = o2_0s
+    
+  if (len(co2_list) > 1):
+    # Set the bit to 1 in the pivot for comparison.
+    co2_pivot += 2**bit
 
-gamma = 0
-epsilon = 0
+    co2_1s = []
+    co2_0s = []
+    for num in co2_list:
+      if (num >= co2_pivot):
+        co2_1s.append(num)
+      else:
+        co2_0s.append(num)
 
-bit = 0
-for bit_count in bit_counts:
-  if bit_count > 0:
-    gamma += 1 << bit
-  elif bit_count < 0:
-    epsilon += 1 << bit
-  bit += 1
+    if (len(co2_1s) >= len(co2_0s)):
+      # 0s are less common, set the bit to 0 in the pivot.
+      co2_pivot -= 2**bit
+      co2_list = co2_0s
+    else:
+      # 1s are less common, leave the 1 in the pivot.
+      co2_list = co2_1s
 
-print("gamma:   {:d}".format(gamma))
-print("epsilon: {:d}".format(epsilon))
-print("product: {:d}".format(gamma * epsilon))
+o2_value = o2_list[0]
+co2_value = co2_list[0]
+
+print("o2_value:  {0:b} ({0:d})".format(o2_value))
+print("co2_value: {0:b} ({0:d})".format(co2_value))
+print("product:   {:d}".format(o2_value * co2_value))
