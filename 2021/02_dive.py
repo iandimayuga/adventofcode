bfe9f27316a491d1965583adcc874ff1a2236a43
Ian Dimayuga <git@iandimayuga.com>
2021-12-10 23:19:14 -0800

    [Day 2 Part 2] Modify Submarine to use aim instead of ballast.

--- 2021/02_dive.py
+++ 2021/02_dive.py
@@ -13,7 +13,7 @@ _DIRECTION_PARSE = {"forward" : Direction.FORWARD, "down" : Direction.DOWN, "up"
 # A command for the submarine.
 class Command:
   direction = Direction.NONE
-  distance = 0
+  magnitude = 0
   
   def __init__(self, string) -> None:
     # Parse the command from a string such as "forward 3".
@@ -21,22 +21,24 @@ class Command:
     if (command_strings[0] in _DIRECTION_PARSE):
       self.direction = _DIRECTION_PARSE[command_strings[0]]
     if (command_strings[1].isdigit()):
-      self.distance = int(command_strings[1])
+      self.magnitude = int(command_strings[1])
   
   def __repr__(self) -> str:
-    return "{:s} {:d}".format(self.direction, self.distance)
+    return "{:s} {:d}".format(self.direction, self.magnitude)
 
 class Submarine:
+  aim = 0
   horizontal = 0
   depth = 0
   
   def issue_command(self, command: Command) -> None:
     if (command.direction == Direction.FORWARD):
-      self.horizontal += command.distance
+      self.horizontal += command.magnitude
+      self.depth += self.aim * command.magnitude
     elif (command.direction == Direction.DOWN):
-      self.depth += command.distance
+      self.aim += command.magnitude
     elif (command.direction == Direction.UP):
-      self.depth -= command.distance
+      self.aim -= command.magnitude
 
 # Read the input data into a list of commands.
 commands = []
