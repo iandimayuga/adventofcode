6d99564c37957b6fd5876f992bbcc04030b6eab0
Ian Dimayuga <git@iandimayuga.com>
2021-12-10 21:29:59 -0800

    Count the total number of increases in the input data (Day 1 Part 1).

--- 2021/sonarsweep.py
+++ 2021/sonarsweep.py
@@ -1,5 +1,14 @@
+import math
+
 # Test by outputting the input data.
-lines = []
-with open("2021/data/sonarsweep.txt") as input:
-  lines = input.readlines()
-print(lines)
+numlist = []
+with open("2021/data/sonarsweep.txt", "r") as input:
+  numlist = list(map(int, input.readlines()))
+previous = math.inf
+increase_count = 0
+for num in numlist:
+  if num > previous:
+    increase_count += 1
+  previous = num
+
+print("Increased {:d} times".format(increase_count))
