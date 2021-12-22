3f72356634033410646055ce369a86eef1c490e1
Ian Dimayuga <git@iandimayuga.com>
2021-12-22 00:00:00 -0800

    [Day 22 Part 1] Slice the space and find the last command affecting each slice.

--- 2021/22_reactor_reboot.py
+++ 2021/22_reactor_reboot.py
@@ -8,18 +8,25 @@ _GRID_MIN = -50
 _GRID_MAX = 50
 
 class BoundingBox:
-  def __init__(self, range: list[range]) -> None:
-    self.range = range
+  def __init__(self, intervals: list[range]) -> None:
+    self.intervals = intervals
 
   def intersection(self, other: "BoundingBox") -> list[range]:
     return BoundingBox([
       range(max(s[0], o[0]), min(s[-1], o[-1])+1)
-        for (s, o) in zip(self.range, other.range)
+        for (s, o) in zip(self.intervals, other.intervals)
     ])
 
+  def __bool__(self) -> bool:
+    return all(self.intervals)
+
+  def contains(self, point: list[int]) -> bool:
+    return all([interval.__contains__(coordinate)
+      for (interval, coordinate) in zip(self.intervals, point)])
+
   def __repr__(self) -> str:
-    return "x={:s} y={:s} z={:s}".format(
-      str(self.range[0]), str(self.range[1]), str(self.range[2]))
+    return "x={:s}\ty={:s}\tz={:s}".format(
+      str(self.intervals[0]), str(self.intervals[1]), str(self.intervals[2]))
   def __format__(self, __format_spec: str) -> str:
     return self.__repr__()
 
@@ -39,11 +46,39 @@ class Command:
     ])
 
   def __repr__(self) -> str:
-    return "{:s} {:s}".format("ON" if self.on else "off", self.bbox)
+    return "{:s}\t{:s}".format("ON" if self.on else "off", self.bbox)
 
 with open(_DATA_FILE, "r") as input:
   lines = input.readlines()
 
 commands = [Command(line.strip()) for line in lines]
+bounds = [set() for i in range(3)]
+constrained_grid = BoundingBox([range(_GRID_MIN, _GRID_MAX + 1)]*3)
 for command in commands:
   print(command)
+  constrained_bbox = command.bbox.intersection(constrained_grid)
+  if (constrained_bbox):
+    for (bound, interval) in zip(bounds, constrained_bbox.intervals):
+      bound.add(interval.start)
+      bound.add(interval.stop)
+
+x_bounds = sorted(bounds[0])
+y_bounds = sorted(bounds[1])
+z_bounds = sorted(bounds[2])
+
+total = 0
+for i in range(len(x_bounds) - 1):
+  for j in range(len(y_bounds) - 1):
+    for k in range(len(z_bounds) - 1):
+      (x, y, z) = (x_bounds[i], y_bounds[j], z_bounds[k])
+      volume = (x_bounds[i + 1] - x) * (y_bounds[j + 1] - y) * (z_bounds[k + 1] - z)
+      print("Volume of {:d},{:d},{:d}\tto {:d},{:d},{:d}\tis {:d}".format(
+        x,y,z,x_bounds[i+1],y_bounds[j+1],z_bounds[k+1],volume
+      ))
+      for command in reversed(commands):
+        if (command.bbox.contains([x,y,z])):
+          if (command.on):
+            total += volume
+          break
+
+print("Total:", total)
