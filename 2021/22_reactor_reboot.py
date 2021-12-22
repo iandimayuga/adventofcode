2b51ef6f8ec905c99f7fc483854f0b93191f0096
Ian Dimayuga <git@iandimayuga.com>
2021-12-22 01:10:58 -0800

    [Day 22 Part 2] Divide the reactor cube space into smaller parallelizable cells.

--- 2021/22_reactor_reboot.py
+++ 2021/22_reactor_reboot.py
@@ -1,4 +1,8 @@
-_DATA_FILE = "2021/data/test/22_reactor_reboot_2.txt"
+from multiprocessing import Pool
+from itertools import product
+import math
+
+_DATA_FILE = "2021/data/22_reactor_reboot.txt"
 _COMMAND_DELIMITER = ' '
 _ON_COMMAND = "on"
 _COORD_DELIMITER = ','
@@ -6,16 +10,21 @@ _COORD_OFFSET = 2
 _RANGE_DELIMITER = '..'
 _GRID_MIN = -50
 _GRID_MAX = 50
+_CELL_SIZE = 10000
+_POOL_SIZE = 16
 
 class BoundingBox:
   def __init__(self, intervals: list[range]) -> None:
     self.intervals = intervals
 
   def intersection(self, other: "BoundingBox") -> list[range]:
-    return BoundingBox([
-      range(max(s[0], o[0]), min(s[-1], o[-1])+1)
-        for (s, o) in zip(self.intervals, other.intervals)
-    ])
+    if (self and other):
+      return BoundingBox([
+        range(max(s[0], o[0]), min(s[-1], o[-1])+1)
+          for (s, o) in zip(self.intervals, other.intervals)
+      ])
+    
+    return BoundingBox(range(0) for i in range(3))
 
   def __bool__(self) -> bool:
     return all(self.intervals)
@@ -48,37 +57,76 @@ class Command:
   def __repr__(self) -> str:
     return "{:s}\t{:s}".format("ON" if self.on else "off", self.bbox)
 
-with open(_DATA_FILE, "r") as input:
-  lines = input.readlines()
+class CellArgs:
+  def __init__(self, commands: list[Command], cell_min: list[int], cell_max: list[int]) -> None:
+    self.commands = commands
+    self.cell_min = cell_min
+    self.cell_max = cell_max
 
-commands = [Command(line.strip()) for line in lines]
-bounds = [set() for i in range(3)]
-constrained_grid = BoundingBox([range(_GRID_MIN, _GRID_MAX + 1)]*3)
-for command in commands:
-  print(command)
-  constrained_bbox = command.bbox.intersection(constrained_grid)
-  if (constrained_bbox):
-    for (bound, interval) in zip(bounds, constrained_bbox.intervals):
-      bound.add(interval.start)
-      bound.add(interval.stop)
+def process_cell(args: CellArgs) -> int:
+  commands = args.commands
+  cell_min = args.cell_min
+  cell_max = args.cell_max
 
-x_bounds = sorted(bounds[0])
-y_bounds = sorted(bounds[1])
-z_bounds = sorted(bounds[2])
+  print("Processing cell", str(cell_min), str(cell_max))
 
-total = 0
-for i in range(len(x_bounds) - 1):
-  for j in range(len(y_bounds) - 1):
-    for k in range(len(z_bounds) - 1):
-      (x, y, z) = (x_bounds[i], y_bounds[j], z_bounds[k])
-      volume = (x_bounds[i + 1] - x) * (y_bounds[j + 1] - y) * (z_bounds[k + 1] - z)
-      print("Volume of {:d},{:d},{:d}\tto {:d},{:d},{:d}\tis {:d}".format(
-        x,y,z,x_bounds[i+1],y_bounds[j+1],z_bounds[k+1],volume
-      ))
-      for command in reversed(commands):
-        if (command.bbox.contains([x,y,z])):
-          if (command.on):
-            total += volume
-          break
+  bounds = [set() for i in range(3)]
+  cell = BoundingBox([range(min, max) for (min, max) in zip(cell_min, cell_max)])
+  for command in commands:
+    # print(command)
+    constrained_bbox = command.bbox.intersection(cell)
+    if (constrained_bbox):
+      for (bound, interval) in zip(bounds, constrained_bbox.intervals):
+        bound.add(interval.start)
+        bound.add(interval.stop)
 
-print("Total:", total)
+  x_bounds = sorted(bounds[0])
+  y_bounds = sorted(bounds[1])
+  z_bounds = sorted(bounds[2])
+
+  total = 0
+  for i in range(len(x_bounds) - 1):
+    for j in range(len(y_bounds) - 1):
+      for k in range(len(z_bounds) - 1):
+        (x, y, z) = (x_bounds[i], y_bounds[j], z_bounds[k])
+        volume = (x_bounds[i + 1] - x) * (y_bounds[j + 1] - y) * (z_bounds[k + 1] - z)
+        # print("Volume of {:d},{:d},{:d}\tto {:d},{:d},{:d}\tis {:d}".format(
+        #   x,y,z,x_bounds[i+1],y_bounds[j+1],z_bounds[k+1],volume
+        # ))
+        for command in reversed(commands):
+          if (command.bbox.contains([x,y,z])):
+            if (command.on):
+              total += volume
+            break
+
+  return total
+
+if __name__ == '__main__':
+  with open(_DATA_FILE, "r") as input:
+    lines = input.readlines()
+
+  commands = [Command(line.strip()) for line in lines]
+  # print(process_cell(CellArgs(commands, _GRID_MIN, _GRID_MAX + 1)))
+
+  grid_min = [math.inf]*3
+  grid_max = [-math.inf]*3
+
+  for command in commands:
+    for i in range(3):
+      grid_min[i] = min(grid_min[i], command.bbox.intervals[i].start)
+      grid_max[i] = max(grid_max[i], command.bbox.intervals[i].stop)
+  
+  print(grid_min)
+  print(grid_max)
+
+  cells = []
+  for cell_x in range(grid_min[0], grid_max[0], _CELL_SIZE):
+    for cell_y in range(grid_min[1], grid_max[1], _CELL_SIZE):
+      for cell_z in range(grid_min[2], grid_max[2], _CELL_SIZE):
+        cell_min = [cell_x, cell_y, cell_z]
+        cell_max = [cell_x + _CELL_SIZE, cell_y + _CELL_SIZE, cell_z + _CELL_SIZE]
+        print("Creating cell", cell_min, cell_max)
+        cells.append(CellArgs(commands, cell_min, cell_max))
+
+  with Pool(_POOL_SIZE) as p:
+      print(sum(p.map(process_cell, cells)))
