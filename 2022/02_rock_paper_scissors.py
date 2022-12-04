0086ed9d9b29efb36eb1373217635080e4b1b6c4
Ian Dimayuga <git@iandimayuga.com>
2022-12-03 16:38:50 -0800

    [2022 Day 2 Part 2] Reinterpret second column as the desired outcome and derive the desired throw.

--- 2022/02_rock_paper_scissors.py
+++ 2022/02_rock_paper_scissors.py
@@ -7,6 +7,9 @@ _LOSE_SCORE = 0
 _DRAW_SCORE = 3
 _WIN_SCORE = 6
 
+_LOSE_OUTCOME_LETTER = 'X'
+_LOSE_OUTCOME_MOD_DIFF = 2
+
 def me_throw_score(letter: str) -> int:
   return ord(letter) - ord(_ROCK_LETTER_ME) + _ROCK_SCORE
 
@@ -20,11 +23,21 @@ def rps_score(them: str, me: str) -> int:
   # You draw if you're equal (0 mod 3).
   mod_diff = (me_throw_score(me) - them_throw_score(them)) % 3
   if mod_diff == 2:
-    return _LOSE_SCORE
+    return _LOSE_SCORE + me_throw_score(me)
   elif mod_diff == 1:
-    return _WIN_SCORE
+    return _WIN_SCORE + me_throw_score(me)
   elif mod_diff == 0:
-    return _DRAW_SCORE
+    return _DRAW_SCORE + me_throw_score(me)
+
+# Determine the desired mod-diff based on the prescribed outcome.
+def outcome_mod_diff(letter: str) -> int:
+  return (ord(letter) - ord(_LOSE_OUTCOME_LETTER) + _LOSE_OUTCOME_MOD_DIFF) % 3
+
+def needed_throw(them: str, outcome: str) -> str:
+  # me - them = outcome, mod 3
+  # me = outcome + them, mod 3
+  me_throw_score = (outcome_mod_diff(outcome) + them_throw_score(them)) % 3
+  return chr((me_throw_score - _ROCK_SCORE) % 3 + ord(_ROCK_LETTER_ME))
 
 games = []
 
@@ -35,9 +48,16 @@ total_score = 0
 
 for game in games:
   them = game[0]
-  me = game[1]
-  score = rps_score(them, me) + me_throw_score(me)
-  print(str(score))
+  outcome = game[1]
+  me = needed_throw(them, outcome)
+  score = rps_score(them, me)
+  print("Them: {them:s} Me: {me:s} Outcome: {outcome_mod_diff:d} Throw Score: {me_throw_score:d} RPS Score: {rps_score:d}".format(
+    them = them,
+    me = me,
+    outcome_mod_diff = outcome_mod_diff(outcome),
+    me_throw_score = me_throw_score(me),
+    rps_score = rps_score(them, me)
+  ))
   total_score += score
 
 print("Total: {t:d}".format(t = total_score))
