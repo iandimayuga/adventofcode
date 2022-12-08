8a4dc2e73a39ad1e2e53b5bfec775a1a8928a317
Ian Dimayuga <git@iandimayuga.com>
2022-12-07 21:56:37 -0800

    [2022 Day 8 Part 2] Find the visibility from the top of each tree.

--- 2022/08_treetop_tree_house.py
+++ 2022/08_treetop_tree_house.py
@@ -1,60 +1,47 @@
 _DATA_FILE = "2022/data/08_treetop_tree_house.txt"
 
+def scenic_score(grid, row, col) -> int:
+  height = grid[row][col]
+  # Look down
+  down_score = 0
+  for i in range(row + 1, len(grid)):
+    down_score += 1
+    if height <= grid[i][col]:
+      break
+  # Look up
+  up_score = 0
+  for i in reversed(range(0, row)):
+    up_score += 1
+    if height <= grid[i][col]:
+      break
+  # Look right
+  right_score = 0
+  for j in range(col + 1, len(grid[0])):
+    right_score += 1
+    if height <= grid[row][j]:
+      break
+  # Look left
+  left_score = 0
+  for j in reversed(range(0, col)):
+    left_score += 1
+    if height <= grid[row][j]:
+      break
+
+  return down_score * up_score * right_score * left_score
+
+
+
 grid = []
 with open(_DATA_FILE, "r") as input:
   for line in input:
     grid.append([int(c) for c in line.strip()])
 
-print(grid)
-
-visible = [[False] * len(grid[0]) for i in range(len(grid))]
-
-for i in range(len(grid)):
-  running_max = -1
-  for j in range(len(grid[0])):
-    height = grid[i][j]
-    if height > running_max:
-      visible[i][j] = True
-      print("{height:d} at {i:d},{j:d} is visible from left".format(
-        height = height, i = i, j = j
-      ))
-    running_max = max(running_max, height)
-
-  running_max = -1
-  for j in reversed(range(len(grid[0]))):
-    height = grid[i][j]
-    if height > running_max:
-      visible[i][j] = True
-      print("{height:d} at {i:d},{j:d} is visible from right".format(
-        height = height, i = i, j = j
-      ))
-    running_max = max(running_max, height)
-
-for j in range(len(grid[0])):
-  running_max = -1
-  for i in range(len(grid)):
-    height = grid[i][j]
-    if height > running_max:
-      visible[i][j] = True
-      print("{height:d} at {i:d},{j:d} is visible from above".format(
-        height = height, i = i, j = j
-      ))
-    running_max = max(running_max, height)
-
-  running_max = -1
-  for i in reversed(range(len(grid))):
-    height = grid[i][j]
-    if height > running_max:
-      visible[i][j] = True
-      print("{height:d} at {i:d},{j:d} is visible from below".format(
-        height = height, i = i, j = j
-      ))
-    running_max = max(running_max, height)
-
-num_visible = 0
+max_score = 0
 for i in range(len(grid)):
   for j in range(len(grid[0])):
-    if visible[i][j]:
-      num_visible += 1
+    score = scenic_score(grid, i, j)
+    print(score, end='')
+    max_score = max(max_score, score)
+  print()
 
-print(num_visible)
+print(max_score)
