cbf569e7f4230d57bd7e64f8e9dfe4e754021591
Ian Dimayuga <git@iandimayuga.com>
2022-12-16 22:11:20 -0800

    [2022 Day 16 Part 1] Try memoizing best pressures with tuple key.
    This is not performing well, I'm going to try concatenating to a string instead.

--- 2022/16_proboscidea_volcanium.py
+++ 2022/16_proboscidea_volcanium.py
@@ -1,20 +1,25 @@
 import re
 
-_DEBUG = True
+_PART = 1
+_DEBUG = False
 _DATA_FOLDER = "2022/data/test/" if _DEBUG else "2022/data/"
 _DATA_FILE = _DATA_FOLDER + "16_proboscidea_volcanium.txt"
 
+# Parsing constants.
 _VALVE_PATTERN = re.compile(
   r"^Valve (?P<name>\w+) has flow rate=(?P<rate>\d+); " +
   r"tunnel(s)? lead(s)? to valve(s)? (?P<neighbors>.+)$")
 _NEIGHBOR_DELIMITER = ', '
+
+# Puzzle state
+_TOTAL_MINUTES = 30
+_FIRST_VALVE = 'AA'
 
 class Valve:
   def __init__(self, name: str, rate: int, neighbors: list[str]) -> None:
     self.name = name
     self.rate = rate
     self.neighbors = neighbors
-    self.open = False
 
   def __str__(self) -> str:
     return "Name={n:s}\tRate={r:d}\tNeighbors={ns:s}".format(
@@ -22,7 +27,6 @@ class Valve:
       r = self.rate,
       ns = ','.join(self.neighbors)
     )
-    
 
 lines = []
 with open(_DATA_FILE, "r") as inputfile:
@@ -38,3 +42,33 @@ for line in lines:
   valves[name] = Valve(name, rate, neighbors)
 
 print('\n'.join([str(valve) for valve in valves.values()]))
+print()
+
+# Memoize the amount of released pressure given a minute, current valve, and set of open valves.
+pressure_cache: dict[tuple[int, str, frozenset[str]], int] = {}
+
+def max_released_pressure(minute: int, current_valve: str, open_valves: frozenset[str]):
+  key = (minute, current_valve, open_valves)
+  if key in pressure_cache:
+    return pressure_cache[key]
+  
+  # Pressure released this minute regardless of action.
+  pressure_released = sum([valves[v].rate for v in open_valves])
+
+  # Explore the space of possible actions.
+  max_benefit = 0
+
+  # No actions possible if this is the last minute.
+  if minute < _TOTAL_MINUTES:
+    # Action: Open current closed valve and stay put.
+    if current_valve not in open_valves:
+      max_benefit = max_released_pressure(minute + 1, current_valve, open_valves | set([current_valve]))
+
+    # Action: Move to another valve.
+    for neighbor in valves[current_valve].neighbors:
+      max_benefit = max(max_benefit, max_released_pressure(minute + 1, neighbor, open_valves))
+
+  pressure_cache[key] = max_benefit + pressure_released
+  return max_benefit + pressure_released
+
+print("Best result: ", max_released_pressure(1, _FIRST_VALVE, frozenset()))
